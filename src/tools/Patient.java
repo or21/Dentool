c0b556d5dabb2a802bcff3105b2ab345100b0a62
or21 <orbrand21@gmail.com>
2015-04-07 12:46:12 +0300

    Begin

--- src/tools/Patient.java
+++ src/tools/Patient.java
@@ -1,5 +1,34 @@
 package tools;
 
 public class Patient {
+	
+	private Tooth[] teeth;
+	private String userName;
+	private String uid;
+	
+	public Patient(String userName, String uid) {
+		this.teeth = new Tooth[32];
+		for (int i = 0; i < 32; i++) {
+			teeth[i] = new Tooth();
+		}
+		this.setUserName(userName);
+		this.setUid(uid);
+	}
 
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	
 }
