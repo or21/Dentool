99c01e8c5061c43ae2d4b073a2be215a64b91ea2
yafim <ifim.vo@gmail.com>
2015-04-07 12:06:40 +0300

    check2

--- src/com/example/dentool/MainActivity.java
+++ src/com/example/dentool/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-	//Yafim
+	//Yafim Or
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
