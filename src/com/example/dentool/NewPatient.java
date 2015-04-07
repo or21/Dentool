28943dbbbc29439f5af2933a5956c7a815064515
or21 <orbrand21@gmail.com>
2015-04-07 15:25:28 +0300

    Or commit

--- src/com/example/dentool/NewPatient.java
+++ src/com/example/dentool/NewPatient.java
@@ -1,39 +1,73 @@
 package com.example.dentool;
 
+import tools.Patient;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class NewPatient extends Activity {
-<<<<<<< HEAD
+public class NewPatient extends Activity implements OnClickListener {
+
+	private static final CharSequence USERNAME = "Please enter patient full name";
+	private static final CharSequence ID = "Please enter patient ID";
+	private Context context;
+	Patient patient;
+	EditText userName;
+	EditText id;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.newpatient_layout);
-=======
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
+		context = getApplicationContext();
+		
+		userName = (EditText) findViewById(R.id.userName);
+		userName.setHint(USERNAME);
+		id = (EditText) findViewById(R.id.id);
+		id.setHint(ID);
+		Button skip = (Button) findViewById(R.id.skip);
+		skip.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(context,Main.class);
+				startActivity(intent);
+			}
+		});
+		Button send = (Button) findViewById(R.id.sendInfo);
+		send.setOnClickListener(this);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onClick(View v) {
+		// TODO: run method that send data to server
+		// if user already treated - load data, else, update and start with empty object
+		Boolean alreadyVisited = false;
+		if (alreadyVisited) {
+			patient = loadPatientData("");
+		}
+		else {
+			patient = initNewPatient();
+		}
+		Intent intent = new Intent(context, BottomFragment1.class);
+		
+		startActivity(intent);
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		return super.onOptionsItemSelected(item);
->>>>>>> origin/master
+	private Patient initNewPatient() {
+		return new Patient(userName.getText().toString(), id.getText().toString());		
 	}
+
+	private Patient loadPatientData(String data) {
+		String newUserName = data;
+		String uid = data;
+		Patient newPatient = new Patient(newUserName, uid);
+		return newPatient;
+	}
+
 }
-//
