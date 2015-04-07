739c9ba541927b9a288a9a4b2c2685714259d680
yafim <ifim.vo@gmail.com>
2015-04-07 15:31:10 +0300

    Main

--- src/com/example/dentool/Main.java
+++ src/com/example/dentool/Main.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-
+//
 public class Main extends FragmentActivity {
 	
 	FragmentPagerAdapter adapter;
